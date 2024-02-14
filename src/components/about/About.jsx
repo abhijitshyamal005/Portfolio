cfa4128529292278e01e2e15935e7f5192f96974
ABHIJIT SHYAMAL <abhijitshyamal005@gmail.com>
2024-02-14 20:49:49 +0530

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./dev2.jpg";
 
 const about = () => {
   return (
-    <div id="resume" className=" container m-auto   mt-16">
+    <div data-aos="fade-up" id="resume" className=" container m-auto   mt-16">
     
       <div data-aos="fade-up" className="relative mb-5">
         <h3 className=" text-3xl font-black text-gray-400 sm:text-2xl">
