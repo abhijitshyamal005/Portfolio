465b524d78dc6fae69c443cc296e85f6cab7eb0f
ABHIJIT SHYAMAL <abhijitshyamal005@gmail.com>
2024-02-15 21:02:34 +0530

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -23,7 +23,7 @@ const Footer = () => {
           <a 
           data-aos="fade-up" data-aos-duration="1200"
 
-            href="https://wa.me/6296390743"
+            href="https://wa.me/+916296390743"
             className="box font-medium text-white  flex items-center justify-center flex-col"
           >
             <IoLogoWhatsapp className=" text-black text-3xl hover:scale-125 cursor-pointer" />
